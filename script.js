52c9d057f79a89637cb3020fbf01bca046ac40ee
Omkar <omkarmraskar@gmail.com>
2023-02-23 23:18:37 +0530

    fixed off margin issue

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ board.addEventListener("mousedown", start)
 board.addEventListener("mouseup", stop);
 board.addEventListener("mousemove", draw);
 
+const svgRect = board.getBoundingClientRect();
 
 function start(event){
     isPainting = true;
     const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    line.setAttribute('x1', event.clientX);
-    line.setAttribute('y1', event.clientY);
-    line.setAttribute('x2', event.clientX);
-    line.setAttribute('y2', event.clientY);
+    line.setAttribute('x1', event.clientX - svgRect.left);
+    line.setAttribute('y1', event.clientY - svgRect.top);
+    line.setAttribute('x2', event.clientX - svgRect.left);
+    line.setAttribute('y2', event.clientY - svgRect.top);
     line.setAttribute('style', "stroke:rgb(0,0,0);stroke-width:3")
     board.appendChild(line);
 }
@@ -23,8 +24,8 @@ function draw(event){
     if(isPainting === false) return;
     boardText.setAttribute('style', 'display: none;');
     const line = board.lastChild;
-    line.setAttribute('x2', event.clientX);
-    line.setAttribute('y2', event.clientY);
+    line.setAttribute('x2', event.clientX - svgRect.left);
+    line.setAttribute('y2', event.clientY - svgRect.top);
 }
 
 function stop(){
