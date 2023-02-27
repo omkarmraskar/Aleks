c5839008b7b9dfa37fba0c2bac73dc7ccc7bb464
Omkar <omkarmraskar@gmail.com>
2023-02-27 13:41:18 +0530

    Snap endpoints of lines together if near

--- script.js
+++ script.js
@@ -18,10 +18,32 @@ function start(event){
         isPainting = true;
         isErasing = false;
         const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
-        line.setAttribute('x1', event.clientX - svgRect.left);
-        line.setAttribute('y1', event.clientY - svgRect.top);
-        line.setAttribute('x2', event.clientX - svgRect.left);
-        line.setAttribute('y2', event.clientY - svgRect.top);
+        let x1 = event.clientX - svgRect.left;
+        let y1 = event.clientY - svgRect.top;
+        let x2 = event.clientX - svgRect.left;
+        let y2 = event.clientY - svgRect.top;
+        line.setAttribute('x1', x1);
+        line.setAttribute('y1', y1);
+        line.setAttribute('x2', x2);
+        line.setAttribute('y2', y2);
+
+        const lines = document.querySelectorAll('line');
+        for(let i=0; i<lines.length; i++){
+            const x1Other = lines[i].getAttribute("x1");
+            const y1Other = lines[i].getAttribute("y1");
+            const x2Other = lines[i].getAttribute("x2");
+            const y2Other = lines[i].getAttribute("y2");
+            const distanceStart = Math.sqrt((x1 - x1Other) ** 2 + (y1 - y1Other) ** 2);
+            const distanceEnd = Math.sqrt((x1 - x2Other) ** 2 + (y1 - y2Other) ** 2);
+            if (distanceStart < 20) {
+                line.setAttribute("x1", x1Other);
+                line.setAttribute("y1", y1Other);
+            }
+            if (distanceEnd < 20) {
+                line.setAttribute("x1", x2Other);
+                line.setAttribute("y1", y2Other);
+              }
+        }
         line.setAttribute('style', "stroke:rgb(0,0,0);stroke-width:2")
         board.appendChild(line);
     }
@@ -40,8 +62,28 @@ function draw(event){
     else if(isPainting === true){
         boardText.setAttribute('style', 'display: none;');
         const line = board.lastChild;
-        line.setAttribute('x2', event.clientX - svgRect.left);
-        line.setAttribute('y2', event.clientY - svgRect.top);
+        const x2 = event.clientX - svgRect.left;
+        const y2 = event.clientY - svgRect.top
+        line.setAttribute('x2', x2);
+        line.setAttribute('y2', y2);
+        const lines = document.querySelectorAll('line');
+        for(let i=0; i<lines.length; i++){
+            const x1Other = lines[i].getAttribute("x1");
+            const y1Other = lines[i].getAttribute("y1");
+            const x2Other = lines[i].getAttribute("x2");
+            const y2Other = lines[i].getAttribute("y2");
+            const distanceStart = Math.sqrt((x2 - x1Other) ** 2 + (y2 - y1Other) ** 2);
+            const distanceEnd = Math.sqrt((x2 - x2Other) ** 2 + (y2 - y2Other) ** 2);
+            if (distanceStart < 20) {
+                line.setAttribute("x2", x1Other);
+                line.setAttribute("y2", y1Other);
+            }
+            if (distanceEnd < 20) {
+                line.setAttribute("x2", x2Other);
+                line.setAttribute("y2", y2Other);
+            }
+
+        }
     }
 }
 
